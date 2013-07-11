8982360bda9fed9665a0c3dabc7fd56f85190a0a
John Smith <example@stanford.edu>
2013-07-11 19:11:23 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buffer = new Buffer(fs.readFileSync('index.html');
+var buffer = new Buffer(fs.readFileSync('index.html'));
 
 var text =  buffer.toString(buffer);
 
